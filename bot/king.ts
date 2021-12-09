8fd05fad9d0de2653feab15a9cc5a417e2f51a0f
Steven Almeroth <fedor@primesite.dev>
2021-12-08 19:00:21 -0500

    connecting

--- bot/king.ts
+++ bot/king.ts
@@ -1,10 +1,62 @@
 
+const ports = {
+  demo: {
+    main: 5124,
+    stream: 5125,
+  },
+  real: {
+    main: 5112,
+    stream: 5113,
+  },
+}
+
+// wss://ws.xtb.com/demo
+// wss://ws.xtb.com/demoStream
+// wss://ws.xtb.com/real
+// wss://ws.xtb.com/realStream
+const host = 'ws.xtb.com'
+const type = 'demo'
+// const port = ports[type].main
+// const url = `wss://${host}:${port}/${type}`
+const url = `wss://${host}/${type}`
+let socket: WebSocket
+
+function connect() {
+  console.log('Connecting with', url)
+  socket = new WebSocket(url);
+  // socket.addEventListener('open', function (event: any) {
+  //   console.log('open', event)
+  //   socket.send('{ "command": "ping" }');
+  // });
+  // socket.addEventListener('message', function (event: any) {
+  //   console.log('Message from server ', event.data);
+  // });
+  socket.onopen = console.log
+  socket.onclose = console.log
+  socket.onmessage = (message: any) => {
+    console.log(message.data)
+  }
+  console.log('socket', socket)
+}
+
+function ping() {
+  socket.send('{ "command": "ping" }');
+}
+
+function close() {
+  console.log('Closing')
+  socket.close()
+}
+
 type KeyMap = {
   [key: string]: () => void
 }
 
 const cmdFunctions: KeyMap = {
   0 : () => console.log('All systems normal'),
+  1 : connect,
+  2 : close,
+  3 : ping,
 }
 
 function printCommands(): void {
@@ -28,7 +80,8 @@ async function* getInput (): AsyncGenerator<string, void, void> {
 
 async function* getCommandFunction (): AsyncGenerator<()=>void, void, void> {
   for await (const input of getInput()) {
-    yield input in cmdFunctions ? cmdFunctions[ input ] : printCommands
+    if (input) yield input in cmdFunctions ? cmdFunctions[ input ] : printCommands
+    else console.log()
   }
 }
 
