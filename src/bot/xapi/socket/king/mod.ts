e7ea7cdca6085fff7b508f4a9e0516ed1f8765c3
Steven Almeroth <fedor@primesite.dev>
2021-12-14 13:35:48 -0500

    send module

--- src/bot/xapi/socket/king/mod.ts
+++ src/bot/xapi/socket/king/mod.ts
@@ -7,6 +7,7 @@ import { State } from '../const.ts'
 import Socket from '../mod.ts' // XXX TODO Circular reference
 
 import { InputData, KingResponse, XapiDataResponse } from './mod.d.ts'
+import { sendx, sync } from './send.ts'
 import status from './status.ts'
 import print from './print.ts'
 
@@ -15,6 +16,8 @@ export default class KingSocket extends WebSocket {
   session = ''
   status = status
   print = print
+  sendx = sendx
+  sync = sync
 
   constructor (url: string) {
     super(url)
@@ -48,46 +51,6 @@ export default class KingSocket extends WebSocket {
 
   get isOpen (): boolean {
     return this.readyState === State.OPEN
-  }
-
-  sendx (data: InputData): void {
-    if (this.isOpen) {
-      this.send(JSON.stringify(data))
-    }
-  }
-
-  async sync (data: InputData): Promise<KingResponse> {
-    // TODO Also check for logged in
-    if (!this.isOpen) { return { status: false, errorCode: 'K1NG', errorDescr: 'Closed' }}
-    const customTag = Math.random().toString()
-    const _data = Object.assign({ customTag }, data)
-    let timeout = 0
-    // deno-lint-ignore no-explicit-any
-    let result: any
-
-    Logger.info('Syncing', JSON.stringify(_data))
-
-    const listener = (message: MessageEvent) => {
-      const mData = JSON.parse(message.data)
-      if (mData.customTag === customTag) {
-        result = mData
-        this.removeEventListener('message', listener)
-      }
-    }
-    this.addEventListener('message', listener)
-    this.sendx(_data)
-
-    while (!result) {
-      if (++timeout > 10) {
-        result = { status: false, errorCode: 'K1NG', errorDescr: 'Timeout' }
-      }
-      else {
-        console.log('wait')
-        await new Promise(res => setTimeout(res, 200)) // Sleep
-      }
-    }
-
-    return result
   }
 
   async trades (): Promise<void> {
---
+++ src/bot/xapi/socket/king/send.ts
@@ -0,0 +1,44 @@
+import Logger from '../../../../log.ts'
+
+import { InputData, KingResponse } from './mod.d.ts'
+import KingSocket from './mod.ts'
+
+export function sendx (this: KingSocket, data: InputData) {
+  if (this.isOpen) {
+    this.send(JSON.stringify(data))
+  }
+}
+
+export async function sync (this: KingSocket, data: InputData): Promise<KingResponse> {
+  // TODO Also check for logged in
+  if (!this.isOpen) { return { status: false, errorCode: 'K1NG', errorDescr: 'Closed' }}
+  const customTag = Math.random().toString()
+  const _data = Object.assign({ customTag }, data)
+  let timeout = 0
+  // deno-lint-ignore no-explicit-any
+  let result: any
+
+  Logger.info('Syncing', JSON.stringify(_data))
+
+  const listener = (message: MessageEvent) => {
+    const mData = JSON.parse(message.data)
+    if (mData.customTag === customTag) {
+      result = mData
+      this.removeEventListener('message', listener)
+    }
+  }
+  this.addEventListener('message', listener)
+  this.sendx(_data)
+
+  while (!result) {
+    if (++timeout > 10) {
+      result = { status: false, errorCode: 'K1NG', errorDescr: 'Timeout' }
+    }
+    else {
+      console.log('wait')
+      await new Promise(res => setTimeout(res, 200)) // Sleep
+    }
+  }
+
+  return result
+}
