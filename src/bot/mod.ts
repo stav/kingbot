4b1605f7226438b93e37c49c74ddf964f3d0049e
Steven Almeroth <fedor@primesite.dev>
2021-12-18 23:20:36 -0500

    tweak

--- src/bot/mod.ts
+++ src/bot/mod.ts
@@ -7,14 +7,9 @@ const kingcount = new KingCount()
 async function start (): Promise<void> {
   for await (const input of getInput(kingcount)) {
     const obj = Commands.getBinding(kingcount, input)
-    if (obj) {
-      console.info(obj)
-      if (typeof obj === 'function')
-        obj()
-    }
-    else {
-      console.info()
-    }
+    console.info(obj)
+    if (typeof obj === 'function')
+      obj()
   }
 }
 
