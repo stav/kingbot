56f2e6c04adb350d893016b8acf5a28eb9538044
Steven Almeroth <fedor@primesite.dev>
2021-12-27 14:33:53 -0500

    sign module

--- src/bot/kucoin/kuconn.ts
+++ src/bot/kucoin/kuconn.ts
@@ -1,32 +1,12 @@
-import { encode as b64encode } from 'https://deno.land/std/encoding/base64.ts'
-
 import type { ConfigAccount } from '../config.d.ts'
 import type { KingConn } from '../conn.d.ts'
 import Socket from '../socket.ts'
+import sign from './sign.ts'
 
 type ApiKeyData = {
   key: string
   secret: string
   passphrase: string
-}
-
-async function importKey (secret: string): Promise<CryptoKey> {
-  return await crypto.subtle.importKey(
-                                'raw', //      format: "raw" | "pkcs8" | "spki"
-     new TextEncoder().encode(secret), //     keyData: BufferSource
-    { name: 'HMAC', hash: 'SHA-256' }, //   algorithm: AlgorithmIdentifier
-                               false , // extractable: boolean
-                             ['sign'], //   keyUsages: KeyUsage[ sign, verify ]
-  )
-}
-
-async function sign (data: string, secret: string): Promise<string> {
-  // https://stackoverflow.com/questions/65805172#answer-65807243
-  const key: CryptoKey = await importKey(secret)
-  const encodedData = new TextEncoder().encode(data).buffer
-  const signedData = await crypto.subtle.sign('HMAC', key , encodedData)
-  const signature = b64encode(new Uint8Array(signedData))
-  return signature
 }
 
 export default class KuConn extends Socket implements KingConn {
