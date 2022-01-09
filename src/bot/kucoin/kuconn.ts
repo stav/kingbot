7bdc5e6c3f6ec65c3ccedf7b82dad311e87de8c9
Steven Almeroth <fedor@primesite.dev>
2022-01-08 22:06:07 -0500

    kucoin resolve module

--- src/bot/kucoin/kuconn.ts
+++ src/bot/kucoin/kuconn.ts
@@ -1,13 +1,7 @@
 import type { KucoinConfigAccount } from '../config.d.ts'
 import type { KingConn } from '../conn.d.ts'
 import Socket from '../socket.ts'
-import sign from './sign.ts'
-
-type ApiKeyData = {
-  key: string
-  secret: string
-  passphrase: string
-}
+import { resolvePrivate, resolvePublic } from './resolve.ts'
 
 export default class KuConn extends Socket implements KingConn {
 
@@ -20,6 +14,9 @@ export default class KuConn extends Socket implements KingConn {
   // tickPrice = ''
   // tickTime = 0
 
+  resolvePublic = resolvePublic
+  resolvePrivate = resolvePrivate
+
   constructor (account: KucoinConfigAccount) {
     super()
     this.account = account
@@ -27,44 +24,6 @@ export default class KuConn extends Socket implements KingConn {
 
   private get scribe () {
     return this.#tickToggle ? 'unsubscribe' : 'subscribe'
-  }
-
-  private async resolvePublic (relativePath: string, options?: RequestInit) {
-    const url = this.base + '/api/v1/' + relativePath
-    const response = await fetch(url, options)
-    return response.ok
-      ? (await response.json()).data
-      : response
-  }
-
-  async auth(api: ApiKeyData, method: string, url: string, body = '') {
-    const timestamp = Date.now()
-    const document = timestamp + method.toUpperCase() + url + body
-    return {
-      'KC-API-KEY': api.key,
-      'KC-API-SIGN': await sign(document, api.secret),
-      'KC-API-TIMESTAMP': timestamp.toString(),
-      'KC-API-PASSPHRASE': await sign(api.passphrase, api.secret),
-      'KC-API-KEY-VERSION': '2',
-    }
-  }
-
-  private async resolvePrivate (relativePath: string, options?: RequestInit) {
-    const path = '/api/v1/' + relativePath
-    const url = this.base + path
-    const creds = this.account.api
-    const method = options?.method || 'GET'
-    const authHeaders = await this.auth(creds, method, path)
-    const saltHeaders = {
-      'Content-Type': 'application/json',
-      'User-Agent': 'Kingbot/2',
-    }
-    const headers = Object.assign(saltHeaders, authHeaders)
-    const _options = Object.assign({ method, headers }, options)
-    const response = await fetch(url, _options)
-    return response.ok
-      ? (await response.json()).data
-      : response
   }
 
   list (index: number) {
