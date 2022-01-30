5d1ced8dce342bd39547548b50d7e72b8b165277
Steven Almeroth <fedor@primesite.dev>
2022-01-29 21:20:47 -0500

    throttle
    seems like it's working ok

--- src/bot/xapi/socket/send.ts
+++ src/bot/xapi/socket/send.ts
@@ -1,31 +1,87 @@
-import { deadline, delay, DeadlineError } from 'std/async/mod.ts'
+import { deadline, debounce, delay, DeadlineError } from 'std@0.125.0/async/mod.ts'
+import { bgBlack, white, magenta, yellow, red } from 'std@0.125.0/fmt/colors.ts'
 
 import type { InputData, XapiResponse } from './socket.d.ts'
 import type XapiSocket from './socket.ts'
 
 import Logger from 'log'
 
+const TIMEOUT = 2000
+const BURST = 4
+
+let burst = BURST // We get throttled if burst gets down to zero
+let throttled = false
+const queue: string[] = []
+
+function reset() {
+  throttled = false
+  burst = BURST
+}
+const resetDebounced = debounce(reset, TIMEOUT)
+
+const throttleDebounced = debounce(xsocket => {
+  reset()
+  throttle(xsocket)
+}, TIMEOUT)
+
+function throttle(xsocket: WebSocket) {
+  console.log(bgBlack(yellow('THROTTLE')), xsocket?.constructor?.name,
+    'queue', queue.length,
+    'burst', burst,
+    'throttled', throttled,
+  )
+  if (queue.length) {
+    if (throttled) {
+      console.log(bgBlack(red('Throttled, waiting')))
+      throttleDebounced(xsocket)
+    }
+    else {
+      if (burst > 0) {
+        burst--
+        const x = queue.shift() as string
+        console.log(bgBlack(white('Not throttled, sending')), x)
+        xsocket?.send(x)
+        throttle(xsocket)
+      }
+      else {
+        console.log(bgBlack(magenta('Not throttled, but bursted, so throttling')))
+        throttled = true
+        throttle(xsocket)
+      }
+    }
+  }
+  else {
+    resetDebounced()
+  }
+}
+
 /**
  * send
  *
- * Asyncronous send, do not wait for the result.
+ * Send payload throttled via underlying socket.
  *
- * This function requires the underlying socket to be open.
+ * Does not wait for the result.
  *
- * This function becomes a method of the XapiSocket class.
+ * Requires the underlying socket to be open.
+ *
+ * @class Becomes a method of the XapiSocket class.
  *
  * "Each command invocation should not contain more than 1kB of data."
  * "User should send requests in 200 ms intervals. This rule can be broken,
  *  but if it happens 6 times in a row the connection is dropped."
  * @see http://developers.xstore.pro/documentation/#connection-validation
- * @todo Throttle requests
  *
  * @param data The payload to send
  * @returns Nothing
  */
- export function send (this: XapiSocket, data: InputData) {
-  if (this.isOpen) {
-    this.socket?.send(JSON.stringify(data))
+export function send (this: XapiSocket, data: InputData) {
+  if (this.socket && this.isOpen) {
+    const tag = JSON.stringify(data.customTag)
+    const json = JSON.stringify(data)
+    const command = JSON.stringify(data.command)
+    Logger.info('--->SEND<---', json.length, command, tag)
+    queue.push(json)
+    throttle(this.socket)
   }
 }
 
@@ -34,14 +90,14 @@ import Logger from 'log'
  *
  * Syncronous send.
  *
+ * Waits for the result.
+ *
  * 1. Generate a unique string so we can filter responses for the one we send,
  * 2. Send our payload data
  * 2. Listen to messages for the particular one we sent (customTag),
  * 4. Return the result.
  *
- * This function requires the underlying socket to be open.
- *
- * This function becomes a method of the XapiSocket class.
+ * @see send
  *
  * @param data The payload to send
  * @returns The response payload
@@ -73,7 +129,6 @@ export async function sync (this: XapiSocket, data: InputData): Promise<XapiResp
   // 1. Send data
   const customTag = Math.random().toString()
   const _data = Object.assign({ customTag }, data)
-  Logger.info('Syncing', JSON.stringify(_data))
   this.send(_data)
 
   // 2. Wait for Result 2 seconds max
