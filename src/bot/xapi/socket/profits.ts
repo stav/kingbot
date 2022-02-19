20034884b7bb1f33eda1d9477dd89bbfa65f7fe6
Steven Almeroth <fedor@primesite.dev>
2022-02-18 21:19:10 -0500

    std getlogger

--- src/bot/xapi/socket/profits.ts
+++ src/bot/xapi/socket/profits.ts
@@ -1,14 +1,9 @@
-import type {
-  TRADE_RECORD,
-  TRADE_TRANS_INFO,
-  STREAMING_TRADE_RECORD,
-} from '../xapi.d.ts'
+import * as log from 'std/log/mod.ts'
 
+import type { TRADE_RECORD, TRADE_TRANS_INFO, STREAMING_TRADE_RECORD } from '../xapi.d.ts'
 import { CMD_FIELD, TYPE_FIELD } from '../xapi.ts'
 
 import type XapiSocket from './socket.ts'
-
-import Logging from 'lib/logging.ts'
 
 type UpdateOrderEvent = Partial<TRADE_TRANS_INFO>
 
@@ -46,7 +41,7 @@ function isBuyOrder(cmd: number): boolean {
     return data.open_price
   }
   const level = (data.open_price + data.close_price) / 2
-  Logging.logger().info('LEVEL', level, '=', data.open_price, '+', data.close_price, '/', 2)
+  log.getLogger().info('LEVEL', level, '=', data.open_price, '+', data.close_price, '/', 2)
   return level
 }
 
@@ -58,7 +53,7 @@ function isBuyOrder(cmd: number): boolean {
   const margin = level * 0.0003
   const betterment = isBuyOrder(data.cmd) ? +margin : -margin
   const stopLoss = +(level + betterment).toFixed(data.digits)
-  Logging.logger().info('STOP LOSS:', stopLoss, '=', level, '+', betterment)
+  log.getLogger().info('STOP LOSS:', stopLoss, '=', level, '+', betterment)
   return stopLoss
 }
 
@@ -70,7 +65,7 @@ async function setFamilyStoploss( data: STREAMING_TRADE_RECORD,
                                  trades: TRADE_RECORD[],
                                 xsocket: XapiSocket,
 ) {
-  Logging.logger().info('Updating stop loss for', trades.length, 'orders')
+  log.getLogger().info('Updating stop loss for', trades.length, 'orders')
   const transaction: UpdateOrderEvent = {
     type: TYPE_FIELD.MODIFY,
     sl: getStopLoss(data),
--- src/lib/logging.ts
+++ src/lib/logging.ts
@@ -1,39 +1,33 @@
 import * as log from 'std/log/mod.ts'
 
-function logger () {
-  return log.getLogger()
-}
-
 async function setup () {
   // custom configuration with 2 loggers (the default and `tasks` loggers).
   await log.setup({
 
     handlers: {
-      console: new log.handlers.ConsoleHandler("DEBUG"),
+      console: new log.handlers.ConsoleHandler("WARNING"),
 
-      file: new log.handlers.FileHandler("WARNING", {
+      file: new log.handlers.FileHandler("DEBUG", {
         filename: "./logs/kingbot.log",
         formatter: "{datetime} {levelName} {msg}",
       }),
     },
 
     loggers: {
-      // configure default logger available via short-hand methods above.
       default: {
-        level: "DEBUG",
-        handlers: ["file"],
+        level: "NOTSET",
+        handlers: ["file", "console"],
       },
-
-      tasks: {
-        level: "ERROR",
-        handlers: ["console", "file"],
-      },
+      // tasks: {
+      //   level: "ERROR",
+      //   handlers: ["console", "file"],
+      // },
     },
 
   })
+  return log.getLogger()
 }
 
 export default {
-  logger,
   setup,
 }
