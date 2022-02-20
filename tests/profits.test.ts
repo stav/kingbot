8e59edf09077df2026edb41a8767b27bba880683
Steven Almeroth <fedor@primesite.dev>
2022-02-20 02:40:12 -0500

    rhum

--- tests/profits.test.ts
+++ tests/profits.test.ts
@@ -1,5 +1,6 @@
-import { assertEquals } from 'std/testing/asserts.ts'
 import * as logging from 'std/log/mod.ts'
+
+import { Rhum } from 'rhum/mod.ts'
 
 import type { STREAMING_TRADE_RECORD } from 'src/bot/xapi/xapi.d.ts'
 import { testing } from 'src/bot/xapi/socket/profits.ts'
@@ -9,69 +10,84 @@ const { getLevel, getStopLoss } = testing
 
 await logging.setup({ loggers: { default: { level: "WARNING" } } })
 
-Deno.test('profits:getLevel', () => {
+Rhum.testPlan("profits", () => {
 
-  let data = {
-    cmd: CMD_FIELD.BUY,
-    close_price: 2,
-    open_price: 1,
-    sl: 0,
-  } as STREAMING_TRADE_RECORD
-  assertEquals(getLevel(data), 1)
+  Rhum.testSuite("getLevel", () => {
 
-  data = {
-    cmd: CMD_FIELD.BUY,
-    close_price: 3,
-    open_price: 1,
-    sl: 1,
-  } as STREAMING_TRADE_RECORD
-  assertEquals(getLevel(data), 2)
+    Rhum.testCase("should find stop loss for buy tp1", () => {
+      const data = {
+        cmd: CMD_FIELD.BUY,
+        close_price: 2,
+        open_price: 1,
+        sl: 0,
+      } as STREAMING_TRADE_RECORD
+      Rhum.asserts.assertEquals( getLevel(data), 1 )
+    })
+    Rhum.testCase("should find stop loss for buy tp2", () => {
+      const data = {
+        cmd: CMD_FIELD.BUY,
+        close_price: 3,
+        open_price: 1,
+        sl: 1,
+      } as STREAMING_TRADE_RECORD
+      Rhum.asserts.assertEquals( getLevel(data), 2 )
+    })
+    Rhum.testCase("should find stop loss for sell tp1", () => {
+      const data = {
+        cmd: CMD_FIELD.SELL,
+        close_price: 2,
+        open_price: 4,
+        sl: 5,
+      } as STREAMING_TRADE_RECORD
+      Rhum.asserts.assertEquals( getLevel(data), 4 )
+    })
+    Rhum.testCase("should find stop loss for sell tp2", () => {
+      const data = {
+        cmd: CMD_FIELD.SELL,
+        close_price: 1,
+        open_price: 4,
+        sl: 4,
+      } as STREAMING_TRADE_RECORD
+      Rhum.asserts.assertEquals( getLevel(data), 2.5 )
+    })
 
-  data = {
-    cmd: CMD_FIELD.SELL,
-    close_price: 2,
-    open_price: 4,
-    sl: 5,
-  } as STREAMING_TRADE_RECORD
-  assertEquals(getLevel(data), 4)
+  })
 
-  data = {
-    cmd: CMD_FIELD.SELL,
-    close_price: 1,
-    open_price: 4,
-    sl: 4,
-  } as STREAMING_TRADE_RECORD
-  assertEquals(getLevel(data), 2.5)
+  Rhum.testSuite("getStopLoss", () => {
+
+    Rhum.testCase("should calculate stop loss for buy tp1", () => {
+      const data = {
+        cmd: CMD_FIELD.BUY,
+        close_price: 2,
+        open_price: 1,
+        digits: 1,
+        sl: 0,
+      } as STREAMING_TRADE_RECORD
+      Rhum.asserts.assertEquals( getStopLoss(data), 1 )
+    })
+    Rhum.testCase("should calculate stop loss for buy tp2 4 digits", () => {
+      const data = {
+        cmd: CMD_FIELD.BUY,
+        close_price: 3,
+        open_price: 1,
+        digits: 4,
+        sl: 1,
+      } as STREAMING_TRADE_RECORD
+      Rhum.asserts.assertEquals( getStopLoss(data), 2.0006 )
+    })
+    Rhum.testCase("should calculate stop loss for buy tp2 zero digits", () => {
+      const data = {
+        cmd: CMD_FIELD.BUY,
+        close_price: 3,
+        open_price: 1,
+        digits: 0,
+        sl: 1,
+      } as STREAMING_TRADE_RECORD
+      Rhum.asserts.assertEquals( getStopLoss(data), 2 )
+    })
+
+  })
 
 })
 
-Deno.test('profits:getStopLoss', () => {
-
-  let data = {
-    cmd: CMD_FIELD.BUY,
-    close_price: 2,
-    open_price: 1,
-    digits: 1,
-    sl: 0,
-  } as STREAMING_TRADE_RECORD
-  assertEquals(getStopLoss(data), 1)
-
-  data = {
-    cmd: CMD_FIELD.BUY,
-    close_price: 3,
-    open_price: 1,
-    digits: 4,
-    sl: 1,
-  } as STREAMING_TRADE_RECORD
-  assertEquals(getStopLoss(data), 2.0006)
-
-  data = {
-    cmd: CMD_FIELD.BUY,
-    close_price: 3,
-    open_price: 1,
-    digits: 0,
-    sl: 1,
-  } as STREAMING_TRADE_RECORD
-  assertEquals(getStopLoss(data), 2)
-
-})
+Rhum.run()
