782e60d4e3a22094b7ea651b9c28bbc2951d3772
Steven Almeroth <fedor@primesite.dev>
2022-02-26 22:33:30 -0500

    story balance

---
+++ tests/integration/xapi/balance.test.ts
@@ -0,0 +1,33 @@
+import { deadline, delay } from 'std/async/mod.ts'
+import * as logging from 'std/log/mod.ts'
+
+import { Rhum } from 'rhum/mod.ts'
+
+import type { XapiConfigAccount } from 'lib/config.d.ts'
+import config from 'lib/config.ts'
+
+import XConn from 'src/bot/xapi/xconn.ts'
+
+await logging.setup({ loggers: { default: { level: "WARNING" } } })
+
+const TEST_INDEX = 0
+
+Rhum.testSuite('balance', () => {
+
+  const account = config().Accounts[TEST_INDEX] as XapiConfigAccount
+
+  Rhum.testCase('should be a number', async () => {
+    const conn = new XConn(account)
+    const balance = await conn.balance()
+
+    // Close the connection
+    conn.Socket.close()
+    async function wait () { while (!conn.Socket.isClosed) await delay(200) }
+    await deadline(wait(), 2000)
+
+    Rhum.asserts.assertStrictEquals( typeof balance, 'number' )
+  })
+
+})
+
+Rhum.run()
