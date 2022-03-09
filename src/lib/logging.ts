c2badf25ad3ec308eadc9fa20e049c24ed009ef8
Steven Almeroth <fedor@primesite.dev>
2022-03-09 12:53:28 -0500

    better logging setup

--- src/lib/logging.ts
+++ src/lib/logging.ts
@@ -1,8 +1,12 @@
-import type { BaseHandler } from 'std/log/handlers.ts'
 import * as logging from 'std/log/mod.ts'
 import { format } from 'std/datetime/mod.ts'
 
-let handlers = [] as BaseHandler[]
+const level = 'NOTSET' as logging.LevelName
+const loggers = {
+  default: { level, handlers: ["file", "console"] },
+  message: { level, handlers: ["mfile"] },
+  telegram: { level, handlers: ["tfile"] },
+}
 
 const formatter = (logRecord: logging.LogRecord) => {
   const level = logRecord.levelName
@@ -13,55 +17,33 @@ const formatter = (logRecord: logging.LogRecord) => {
     + logRecord.args.map(arg => Deno.inspect(arg)).join(' ')
 }
 
+const handlers = {
+  console: new logging.handlers.ConsoleHandler("WARNING"),
+
+  file: new logging.handlers.FileHandler("DEBUG", {
+    filename: "./logs/kingbot.log",
+    formatter,
+  }),
+
+  mfile: new logging.handlers.FileHandler("NOTSET", {
+    filename: "./logs/kingmsg.log",
+    formatter,
+  }),
+
+  tfile: new logging.handlers.FileHandler("NOTSET", {
+    filename: "./logs/kingram.log",
+    formatter,
+  }),
+}
+const _handlers = Object.values(handlers)
+
 async function setup () {
-  await logging.setup({
-
-    handlers: {
-      console: new logging.handlers.ConsoleHandler("WARNING"),
-
-      file: new logging.handlers.FileHandler("DEBUG", {
-        filename: "./logs/kingbot.log",
-        formatter,
-      }),
-
-      mfile: new logging.handlers.FileHandler("NOTSET", {
-        filename: "./logs/kingmsg.log",
-        formatter,
-      }),
-
-      tfile: new logging.handlers.FileHandler("NOTSET", {
-        filename: "./logs/kingram.log",
-        formatter,
-      }),
-
-    },
-
-    loggers: {
-      default: {
-        level: "NOTSET",
-        handlers: ["file", "console"],
-      },
-      message: {
-        level: "NOTSET",
-        handlers: ["mfile"],
-      },
-      telegram: {
-        level: "NOTSET",
-        handlers: ["tfile"],
-      },
-    },
-
-  })
-  handlers = [
-    ...Array.from(logging.getLogger('default').handlers),
-    ...Array.from(logging.getLogger('message').handlers),
-    ...Array.from(logging.getLogger('telegram').handlers),
-  ]
+  await logging.setup({ handlers, loggers })
   return logging.getLogger('default')
 }
 
 function flush () {
-  handlers.forEach(handler => {
+  _handlers.forEach(handler => {
     if (handler instanceof logging.handlers.FileHandler)
       handler.flush()
   })
