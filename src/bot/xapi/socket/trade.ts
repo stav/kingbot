3f490bffe0ab89b1acccd185537e8592eae8f260
Steven Almeroth <fedor@primesite.dev>
2022-03-22 14:36:28 -0400

    log prices
    during make trades

--- src/bot/xapi/socket/trade.ts
+++ src/bot/xapi/socket/trade.ts
@@ -2,12 +2,22 @@ import { getLogger } from 'std/log/mod.ts'
 
 import Logging from 'lib/logging.ts'
 
-import { REQUEST_STATUS_FIELD } from '../xapi.ts'
-import type { TRADE_RECORD, TRADE_TRANS_INFO, STREAMING_TRADE_STATUS_RECORD } from '../xapi.d.ts'
+import {CMD_FIELD, REQUEST_STATUS_FIELD } from '../xapi.ts'
+import type { TICK_RECORD, TRADE_RECORD, TRADE_TRANS_INFO, STREAMING_TRADE_STATUS_RECORD } from '../xapi.d.ts'
 import type { InputData, XapiResponse, XapiDataResponse } from './socket.d.ts'
 import type XapiSocket from './socket.ts'
 
 type TradeStatus = STREAMING_TRADE_STATUS_RECORD | void
+
+async function getPriceQuotes(this: XapiSocket, trades: TRADE_TRANS_INFO[]) {
+  const args = {
+    level: 0,
+    symbols: trades.map(trade => trade.symbol),
+    timestamp: 0,
+  }
+  const result = await this.fetchCommand('getTickPrices', args)
+  return result.quotations as TICK_RECORD[]
+}
 
 export async function getOpenTrades (this: XapiSocket, openedOnly = false): Promise<TRADE_RECORD[]> {
   let trades: TRADE_RECORD[] = []
@@ -59,10 +69,18 @@ export async function makeTrade(this: XapiSocket, trade: TRADE_TRANS_INFO): Prom
   return statusReturnData
 }
 
+function isBuyOrder(cmd: number): boolean {
+  return ['BUY', 'BUY_LIMIT', 'BUY_STOP'].includes(CMD_FIELD[cmd])
+}
+
 export async function makeTrades (this: XapiSocket, trades: TRADE_TRANS_INFO[]) {
   const klogger = getLogger()
   const tlogger = getLogger('tserver')
   const results = [] as STREAMING_TRADE_STATUS_RECORD[]
+
+  const quotes = await getPriceQuotes.bind(this)(trades)
+  const quote = quotes[0]
+  klogger.info('ServerTradeQuotes', quotes)
 
   for (const trade of trades) {
     const result = await makeTrade.bind(this)(trade) as STREAMING_TRADE_STATUS_RECORD
@@ -72,12 +90,15 @@ export async function makeTrades (this: XapiSocket, trades: TRADE_TRANS_INFO[]) 
     tlogger.info(
       trade.customComment,
       trade.symbol,
-      `@${trade.price}`,
-      `=${trade.tp}`,
+      isBuyOrder(trade.cmd) ? quote?.ask : quote?.bid,
+      CMD_FIELD[trade.cmd],
+      '@', trade.price,
+      '=', trade.tp,
       'order', result.order,
       REQUEST_STATUS_FIELD[result.requestStatus as REQUEST_STATUS_FIELD],
       result.message ? result.message : '',
     )
   }
+  Logging.flush()
   return results
 }
